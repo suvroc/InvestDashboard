f65f2781af50ba9263311ad342211cac082801a1
Adrian <suvroc@o2.pl>
2016-03-03 23:11:35 +0100

    Basic TypeScript test

---
+++ src/main.js
@@ -0,0 +1,12 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello World');
+        alert("aa");
+        return 0;
+    };
+    return Startup;
+})();
+Startup.main();
+//# sourceMappingURL=main.js.map
